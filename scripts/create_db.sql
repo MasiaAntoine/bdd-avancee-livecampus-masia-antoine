da038bbd9d014dc883ba46ec6ed286b0a3cac77e
ThorKild_ <pro.antoine.masia@gmail.com>
2024-11-29 15:10:29 +0100

    correction creation base de donnée

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,27 +1,26 @@
 CREATE DATABASE company;
 
-CREATE TABLE employees(
-   id SERIAL,
-   first_name VARCHAR(75)  NOT NULL,
-   last_name VARCHAR(75)  NOT NULL,
-   email VARCHAR(255)  NOT NULL,
-   salary NUMERIC(15,2)   NOT NULL,
-   PRIMARY KEY(id),
-   UNIQUE(email)
+CREATE TABLE services (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    office_number INT
 );
 
-CREATE TABLE services(
-   id SERIAL,
-   name VARCHAR(255)  NOT NULL,
-   office_number INTEGER NOT NULL,
-   PRIMARY KEY(id)
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    salary NUMERIC(10, 2),
+    service_id INT,
+    CONSTRAINT fk_service FOREIGN KEY (service_id) REFERENCES services (id)
 );
 
-CREATE TABLE manage(
-   employee_id INTEGER,
-   service_id INTEGER,
-   start_date DATE NOT NULL,
-   PRIMARY KEY(employee_id, service_id),
-   FOREIGN KEY(employee_id) REFERENCES employees(id),
-   FOREIGN KEY(service_id) REFERENCES services(id)
+CREATE TABLE manage (
+    service_id INT,
+    employee_id INT,
+    start_date DATE,
+    PRIMARY KEY (service_id, employee_id),
+    CONSTRAINT fk_manage_service FOREIGN KEY (service_id) REFERENCES services (id),
+    CONSTRAINT fk_manage_employee FOREIGN KEY (employee_id) REFERENCES employees (id)
 );
